dbe08779c587595e6b8a19e5e3061074c060f550
Elishaokon <58734811+Elishaokon@users.noreply.github.com>
2023-01-18 09:38:07 +0000

    first

---
+++ elixr/pages/api/index.js
@@ -0,0 +1,24 @@
+import Hero from "./components/Home"
+import Services from "./components/Services"
+import About from "./components/About"
+import Features from "./components/Features"
+import Stats from "./components/Stats"
+import Socials from "./components/Socials"
+import CTA from "./components/CTA"
+import Footer from "./components/Footer"
+import Team from "./components/Team"
+
+export default function Home () {
+    return(
+        <div className="bg-black font-Josefin">
+        <Hero/>
+        <Services/>
+        <About/>
+        <Features/>
+        <Team/>
+        <Stats/>
+        <Socials/>
+        <CTA/>
+        <Footer/>
+        </div>
+    )
